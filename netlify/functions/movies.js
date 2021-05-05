062fe7eb753f4dc78a45020cd8595069634391a1
Andrew Milne <Andrew.milne@kellogg.northwestern.edu>
2021-05-05 10:50:59 -0500

    Session 1

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -22,26 +22,53 @@ exports.handler = async function(event) {
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
   
+  // Conditional that gives error message if year or genre are not provided
   if (year == undefined || genre == undefined) {
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Nope!` // a string of data
+      body: `Please enter a year and genre in the URL` // a string of data
     }
   }
   else {
+    // If year and genre are provided, first build an empty array to be populated
     let returnValue = {
       numResults: 0,
       movies: []
     }
-
+    // Build a for loop that goes through all the movies
+    
     for (let i=0; i < moviesFromCsv.length; i++) {
+      //Build a conditional statement that will add the movie to the ReturnValue array if a match
+      //Store current listing in memory
+      let currentMovie = moviesFromCsv[i]
+    
+      //Check if memory matches year and genre
+      // To do: Ignore if no runtime (genre will already be ignored)
+      
+      if (currentMovie.genres.includes(genre) && currentMovie.startYear==year && currentMovie.runtimeMinutes !== "\\N" && currentMovie.genres.includes("\\N")==false) {
+        // If matches, create post object and populate post object with relevant fields
 
+        postObject = {
+          title: currentMovie.primaryTitle,
+          year: currentMovie.startYear,
+          genres: currentMovie.genres,
+          //runtime: currentMovie.runtimeMinutes //Originally includes to check if any have "//N" in runtime
+        }
+        // Push post object to return array
+        returnValue.movies.push(postObject)
+        // Keep count of how many results are returned
+        //returnValue.numResults++
+      }
+      // Determine the number of results based on length of the array. Other method (counting each time one is added) is commented out above
+      returnValue.numResults = returnValue.movies.length
     }
 
-    // a lambda function returns a status code and a string of data
+    // Return the returnValue array
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+     // Use json stringify to return the returnValue array as a string
+      body: JSON.stringify(returnValue)
+      
     }
   }
 }
